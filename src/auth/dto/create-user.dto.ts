84871238a7d23c6bd15f0b2cd448d0bf9f16d2a1
slobo <suslobo@hotmail.es>
2024-09-11 16:57:33 +0200

    Class Validator - no funciona porque da error por la base de datos Mongoo

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,10 +1,16 @@
+import { IsEmail, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
 
+    //si no viene con estas propiedades no lo acepto
+    @IsEmail()
     email: string;
 
+    @IsString()
     name: string;
 
+    @MinLength(6)
     password: string;
 
-    
+
 }
