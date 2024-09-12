94b1f9a973ef354d9b93b4b5a5a7efc6449a9487
slobo <suslobo@hotmail.es>
2024-09-12 09:46:09 +0200

    auth.services.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new auth';
   }
